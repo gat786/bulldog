f03970a82be0675a58991d578d676afe535feb5b
Ganesh Tiwari <ganesht049@gmail.com>
2026-04-27 15:23:22 +0530

    Added command to print config which is available to bulldog

--- app/main.go
+++ app/main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
-	logrus "gat786/bulldog/log"
-
+	"context"
 	"gat786/bulldog/config"
 	"gat786/bulldog/exporter"
 	"gat786/bulldog/kubernetes"
+	logrus "gat786/bulldog/log"
+	"os"
+
+	// "gat786/bulldog/config"
+	// "gat786/bulldog/exporter"
+	// "gat786/bulldog/kubernetes"
 
 	"github.com/joho/godotenv"
+
+	"github.com/urfave/cli/v3"
 )
 
 func init() {
@@ -18,10 +25,35 @@ func init() {
 	logrus.Info("Initialising Kubernetes Resource Scraper")
 }
 
-func main() {
+func scrape() {
 	outputDir := exporter.GetOutputDir()
 	logrus.Info("Using Output Directory: ", outputDir)
 	logrus.Info("Scraping resources from the cluster")
 	config := config.LoadConfig()
 	kubernetes.GetResources(config, outputDir)
+}
+
+func main() {
+	cmd := &cli.Command{
+		Commands: []*cli.Command{
+			&cli.Command{
+				Name: "scrape",
+				Action: func(ctx context.Context, c *cli.Command) error {
+					logrus.Info("Starting scraping")
+					scrape()
+					return nil
+				},
+			},
+			&cli.Command{
+				Name: "print-config",
+				Action: func(ctx context.Context, c *cli.Command) error {
+					logrus.Info("Printing Scrape config as available to bulldog")
+					return nil
+				},
+			},
+		},
+	}
+	if err := cmd.Run(context.Background(), os.Args); err != nil {
+		logrus.Fatal("Executing Bulldog failed, kindly check the command")
+	}
 }
